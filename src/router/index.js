e6ea5d94735b2bca44a80390c94ef6fa01dd9904
xiaocui <463894536@qq.com>
2019-11-29 16:01:00 +0800

    10

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 const Login = () => import("../views/login/login.vue");
 Vue.use(VueRouter);
-
 const routes = [
-  {
-    path: "/",
-    redirect: "/login"
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: Login
-  }
-]
-
+  { path: "/", redirect: "/login" },
+  { path: "/login", name: "login", component: Login }
+];
 const router = new VueRouter({
   mode: "history",
   routes
 });
-
 export default router;
